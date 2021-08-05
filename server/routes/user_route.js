e53eb4c399e6b084bf52397d318174b5acab37d5
Olukayode <olukayodeshokunbi@gmail.com>
2021-08-05 07:57:23 -0400

    Rename user file to user_schema and create user_model file for user model

--- server/routes/user_route.js
+++ server/routes/user_route.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { hashPassword, comparePassword } = require("../helpers/password_hash");
 const { createAccessToken, createRfreshToken } = require("../helpers/jwt");
-const User = require("../models/user");
+const User = require("../models/user/user");
 const {
   generateVerificationPin,
   verifyPin,
