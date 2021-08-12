204fcfdab63dcc39c70b9362a39380565937d6e5
DKlaver <dklaver15@gmail.com>
2021-08-12 16:52:19 -0500

    Initial commit.

---
+++ client/src/components/pages/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginForm from "../forms/LoginForm";
+
+const Login = () => {
+  return (
+    <div className="bg-gray-50 min-h-1/2 justify-items-center">
+      <LoginForm />
+    </div>
+  );
+};
+
+export default Login;
